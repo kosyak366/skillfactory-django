6d99dc7fd7f4da7a82aafde5ce0d388599eddff8
kosyak366 <terehov366@gmail.com>
2025-03-13 20:30:18 +0300

    Delete my_views.py

--- my_views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.contrib.auth.decorators import login_required
-
-@login_required
-def secret_page(request):
-  return render(request, 'flatpages/default.html', {'flatpage': {'content': 'Секретная информация!'}})
-
-def styled_page(request):
-    return render(request, 'my_app/styled_page.html')
